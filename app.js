fa8a843569dbccdd36d840b74f44cc9c9504a4c6
raj <jajati.raj5@gmail.com>
2023-11-16 21:39:18 +0530

    adding app.js and index.html files

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route to serve the HTML page
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
